6baf4b0199c7cf56a3faef36a8f10727d4d79a15
Jade Ye <JadeYe2020>
2022-01-22 13:39:23 -0400

    Finsih Q2.

--- Assignments/Assignment1B/main.js
+++ Assignments/Assignment1B/main.js
@@ -43,67 +43,74 @@
             return 0;
         } else {           
             
-            // confirm the where the streak starts and how long it is
+            // otherwise confirm where the streak starts and how long it is
             let start = arr[diff.indexOf(1)];
             let len = 2;
 
             // create a 2d array to store the streaks
             const streaks = [];
 
+            // start from the element after the first 1
             for (let i = diff.indexOf(1)+1; i < diff.length; i++) {
-                if (diff[i] === 1) {
-                    len++;
-                    if (start == null) {
-                        start = arr[i]
-                    }
-                } else {
-                    
+                // the current streak breaks or there's no new streak found at this index
+                if (diff[i] !== 1) {
+                    // if the current streak breaks, add the streak parameters into the 2d array
                     if (start != null) {
                         streaks.push([start, len]);
                     }
 
+                    // and then reset the parameters
                     start = null;
                     len = 1;
-                }               
+
+                } else {
+                    // if there's a streak at this index, increment the streak length
+                    len++;
+
+                    // if it's a new streak, update the start value
+                    if (start == null) {
+                        start = arr[i]
+                    }
+                }                        
             }
 
-            console.log(streaks);
-
+            // reset the 2 parameter values based on the first array of 2d array streaks
             start = streaks[0][0];
             len = streaks[0][1];
 
+            // loop through the 2d array to find the longest streak
             for (let i = 0; i < streaks.length; i++) {
+                
                 if (streaks[i][1] > len) {
+                    // if a longer streak is found, update the 2 parameter values
                     len = streaks[i][1];
                     start = streaks[i][0];
                     
                 } else if (streaks[i][1] === len) {
+                    // if there's another streak with the same length, compare the start values
                     if (streaks[i][0] > start) {
+                        // update the start value with a larger one
                         start = streaks[i][0];
                     }
                 }              
             }
 
-            console.log(start);
-            console.log(len);
+            //using the 2 parameter values to calculate the sum
+            return (start + start + len - 1) * len / 2;
         }     
-        console.log(diff);
-
-                
     }
 
+    const arr1 = [1, 2, 3, 6, 9, 34, 2, 6] 
+    console.log("The sum of the longest streak in [" + arr1 + "] is " + sumOfStreak(arr1));
 
-    const arr1 = [3, 2, 7, 5, 6, 7, 3, 8, 9, 10, 23, 2, 1, 2, 3];
-    sumOfStreak(arr1)
-    // console.log("The longest sum of " + arr1 + " = " + sumOfStreak(arr1));
+    const arr2 = [3, 2, 7, 5, 6, 7, 3, 8, 9, 10, 23, 2, 1, 2, 3];
+    console.log("The sum of the longest streak in [" + arr2 + "] is " + sumOfStreak(arr2));
 
-    const arr2 = [100, 101, 102, 3, 4, 5, 6, 9];
-    sumOfStreak(arr2)
-    // console.log("The longest sum of " + arr2 + " = " + sumOfStreak(arr2));
+    const arr3 = [100, 101, 102, 3, 4, 5, 6, 9];
+    console.log("The sum of the longest streak in [" + arr3 + "] is " + sumOfStreak(arr3));
 
-    const arr3 = [9, 8, 7, 6];
-    sumOfStreak(arr3)
-    // console.log("The longest sum of " + arr3 + " = " + sumOfStreak(arr3));
+    const arr4 = [9, 8, 7, 4, 4];
+    console.log("The sum of the longest streak in [" + arr4 + "] is " + sumOfStreak(arr4));
 
 })();
 
