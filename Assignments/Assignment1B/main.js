18a735842788dee31a8b0b98ee9998a955aea45d
Jade Ye <JadeYe2020>
2022-01-22 19:14:30 -0400

    Tidy the code for Q3.

--- Assignments/Assignment1B/main.js
+++ Assignments/Assignment1B/main.js
@@ -118,47 +118,36 @@
 (function(){
     console.log("Question 3:");
 
-    const bDate = new Date(2022, 8, 1, 0, 0, 0);
+    const bDay = new Date(2022, 8, 1, 0, 0, 0);
     const now = new Date();
 
-    console.log(bDate);
-    console.log(now);
+    console.log("Next birthday: " + bDay);
+    console.log("Now: " + now);
 
     // the time left in milliseconds
-    let msToBd = bDate.getTime() - now.getTime();
-    console.log(msToBd);
-
+    let msToBd = bDay.getTime() - now.getTime();
     // the time left in seconds
     let secToBd = Math.round(msToBd / 1000);
-    console.log(secToBd);
 
     // the time left in minutes
     let minToBd = Math.floor(secToBd / 60);
     // record the remained seconds
     secToBd = secToBd % 60
-    console.log(minToBd);
-    console.log(secToBd);
 
     // the time left in hours
     let hrsToBd = Math.floor(minToBd / 60);
     // record the remained minutes
     minToBd %= 60;
-    console.log(hrsToBd);
-    console.log(minToBd);
 
     // the time left in days
     let daysToBd = Math.floor(hrsToBd / 24);
     // record the remained hours
     hrsToBd %= 24;
-    console.log(daysToBd);
-    console.log(hrsToBd);
     
     // the time left in weeks
     let wksToBd = Math.floor(daysToBd / 7);
     // record the remained days
     daysToBd %= 7;
-    console.log(wksToBd);
-    console.log(daysToBd);
     
     console.log("There are " + wksToBd + " weeks, " + daysToBd + " days, " + hrsToBd + " hours, "
      + minToBd + " minutes, and " + secToBd + " seconds until my next birthday!");
