15b2675c6b9c2a0cdc53343ab591aea9f18d843f
Jade Ye <JadeYe2020>
2022-01-24 18:19:07 -0400

    Refactor q2.js' function, not using diff[]

--- Assignments/Assignment1B/q2.js
+++ Assignments/Assignment1B/q2.js
@@ -3,73 +3,69 @@
     console.log("Question 2:");
 
     function sumOfStreak(arr) {
-        // create an array to store the difference between 2 consecutive numbers
-        const diff = [];
 
-        for (let i = 0; i < arr.length-1; i++) {
-            diff.push(arr[i+1] - arr[i]);
+        // create 2 variable to store the first number of a streak and its length
+        let start = null;
+        let len = 1;    //length is 1 when there's no streak
+
+        // create a 2d array to store the streaks
+        const streaks = [];
+        
+        // go through the array and compare each pair of adjacent numbers
+        for (let i = 0; i < arr.length - 1; i++) {
+            
+            if (arr[i+1] - arr[i] === 1) {               
+        
+                // check whether it's a new streak or the current streak extends
+                if (len === 1) {
+                    // update the start value if it's a new streak
+                    start = arr[i];                    
+                } 
+
+                // either way, increment the streak length
+                len++;
+
+            } else { // the current streak breaks or there's no new streak found at this index
+                
+                // if the current streak breaks, add the streak parameters into the 2d array
+                if (len !== 1) {
+                    streaks.push([start, len]);
+                    // and then reset the values of the variables for the next iteration
+                    len = 1;
+                    start = "";
+                }
+            }
         }
 
-        // if there's no streak, return 0
-        if (diff.indexOf(1) === -1) {
+        // confirm whether there are no consecutive numbers in the array
+        if (streaks.length === 0 ) {
+            // the function will return zero if there's no streaks.
             return 0;
-        } else {           
+        }
+
+        // update the 2 parameter values based on the first array of 2d array streaks
+        start = streaks[0][0];
+        len = streaks[0][1];
+
+        // loop through the 2d array to find the longest streak
+        for (let i = 0; i < streaks.length; i++) {
             
-            // otherwise confirm where the streak starts and how long it is
-            let start = arr[diff.indexOf(1)];
-            let len = 2;
-
-            // create a 2d array to store the streaks
-            const streaks = [];
-
-            // start from the element after the first 1
-            for (let i = diff.indexOf(1)+1; i < diff.length; i++) {
-                // the current streak breaks or there's no new streak found at this index
-                if (diff[i] !== 1) {
-                    // if the current streak breaks, add the streak parameters into the 2d array
-                    if (start != null) {
-                        streaks.push([start, len]);
-                    }
-
-                    // and then reset the parameters
-                    start = null;
-                    len = 1;
-
-                } else {
-                    // if there's a streak at this index, increment the streak length
-                    len++;
-
-                    // if it's a new streak, update the start value
-                    if (start == null) {
-                        start = arr[i]
-                    }
-                }                        
-            }
-
-            // reset the 2 parameter values based on the first array of 2d array streaks
-            start = streaks[0][0];
-            len = streaks[0][1];
-
-            // loop through the 2d array to find the longest streak
-            for (let i = 0; i < streaks.length; i++) {
+            if (streaks[i][1] > len) {
+                // if a longer streak is found, update the 2 parameter values
+                len = streaks[i][1];
+                start = streaks[i][0];
                 
-                if (streaks[i][1] > len) {
-                    // if a longer streak is found, update the 2 parameter values
-                    len = streaks[i][1];
+            } else if (streaks[i][1] === len) {
+                // if there's another streak with the same length, compare the start values
+                if (streaks[i][0] > start) {
+                    // update the start value with a larger one
                     start = streaks[i][0];
-                    
-                } else if (streaks[i][1] === len) {
-                    // if there's another streak with the same length, compare the start values
-                    if (streaks[i][0] > start) {
-                        // update the start value with a larger one
-                        start = streaks[i][0];
-                    }
-                }              
-            }
+                }
+            }              
+        }
 
-            //using the 2 parameter values to calculate the sum
-            return (start + start + len - 1) * len / 2;
-        }     
+        //using the 2 parameter values to calculate the sum
+        return (start + start + len - 1) * len / 2;        
     }
 
     const arr1 = [1, 2, 3, 6, 9, 34, 2, 6] 
