3582604ea487dc18e22dbca7bafee59412e199a3
Jade Ye <JadeYe2020>
2022-02-03 10:56:44 -0400

    New in-class demo

---
+++ Demos/APIDemo/main.js
@@ -0,0 +1,14 @@
+(function() {
+
+    // Say "Hello."
+    document.write("Hello.<br/>");
+
+    // Say "Goodbye" two seconds from now.
+    setTimeout(function() {
+        document.write("Goodbye!");
+    }, 2000);
+
+    // Say "Hello again!"
+    document.write("Hello again!<br/>");
+
+}) ();
