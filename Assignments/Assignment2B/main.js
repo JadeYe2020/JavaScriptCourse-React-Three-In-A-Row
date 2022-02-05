51701b4a7270afb66fdbdb347cf60f25face0686
Jade Ye <JadeYe2020>
2022-02-05 19:33:15 -0400

    Make a table with poster images and lists of genres.

--- Assignments/Assignment2B/main.js
+++ Assignments/Assignment2B/main.js
@@ -4,13 +4,23 @@
     fetch("https://imdb-api.com/en/API/InTheaters/k_t74rh7ze")
     .then(response => response.json())
     .then(data => {
-        var output = "<ul>";
+        var output = "<tr><th>Name</th><th>Directed by</th><th>Genre</th><th>Stars</th></tr>";
         
         data.items.forEach(function(item) {
-            output += "<li>" + item.title + "</li>";
+            output += "<tr>";
+            output += "<td><em>" + item.title + "</em><br/><img src=" + item.image + "></td>";
+            output += "<td>" + item.directors + "</td>";
+            output += "<td><ul>";
+
+            item.genreList.forEach(genre => {
+                output += "<li>" + genre.value + "</li>";
+            });
+
+            output += "</ul></td>";
+            output += "<td>" + item.stars + "</td>";
+            output += "</tr>";
         });
 
-        output += "</ul>"
 
         document.querySelector("#list").innerHTML = output;
     });
