8f1e7a0dc688a68a676b60cf06405247522a2165
Jade Ye <JadeYe2020>
2022-02-14 10:41:14 -0400

    Create function rankHand(hand). Test sort value ok.

--- Assignments/Assignment2C/main.js
+++ Assignments/Assignment2C/main.js
@@ -1,5 +1,20 @@
 (() => {
     
+    function rankHand(hand) {
+        const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+        const suits = ['D', 'C', 'H', 'S'];
+        
+        hand = [['Q', 'H'], ['A', 'H'], ['8', 'H'], ['K', 'H'], ['4', 'H']];
+        const valueIndexes = [];
+
+        for (let i = 0; i < hand.length; i++) {            
+            valueIndexes.push(values.indexOf(hand[i][0]));
+        }
+
+        valueIndexes.sort((a, b) => a - b); // reference: https://www.w3schools.com/js/js_array_sort.asp
+        console.log(valueIndexes);
+    }
+    
     const deckAPIurl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
     const numCards = 5;    
     
@@ -7,7 +22,7 @@
     .then(response => response.json())
     .then(data => {
         // use the deck_id to form a new api url to get 5 cards
-        const cardsUrl = 'https://deckofcardsapi.com/api/deck/' + data.deck_id + '/draw/?count=' + numCards;
+        // const cardsUrl = 'https://deckofcardsapi.com/api/deck/' + data.deck_id + '/draw/?count=' + numCards;
 
         // Royal Flush
         // const cardsUrl = "http://pokerhand-tester.herokuapp.com/royalflush";
@@ -18,7 +33,7 @@
         // Full House
         // const cardsUrl = "http://pokerhand-tester.herokuapp.com/fullhouse";
         // Flush
-        // const cardsUrl = "http://pokerhand-tester.herokuapp.com/flush";
+        const cardsUrl = "http://pokerhand-tester.herokuapp.com/flush";
         // Straight
         // const cardsUrl = "http://pokerhand-tester.herokuapp.com/straight";
         // Three of a kind
@@ -46,6 +61,10 @@
 
             htmlOutput += "<img src='" + card.image + "'/>";                
         });
+
+        console.log(myCards);
+
+        rankHand(myCards);
 
         document.querySelector("#cards").innerHTML = htmlOutput;
 
