1c678325335fbe401102729a6449606f392380b2
Jade Ye <JadeYe2020>
2022-02-17 10:49:51 -0400

    start an in-class demo

---
+++ Demos/ArrayFunctions/main.js
@@ -0,0 +1,17 @@
+(() => {
+
+    // array manipulation functions
+    const isEveryoneOfAge = () => {
+        
+    };
+
+    // my original array
+    const myArray = [
+        { name: "Thomas", age:19 },
+        { name: "Noe", age:17 },
+        { name: "Luc", age:20 },
+    ];
+
+    // a function to check if everyone is of age >= 18
+
+})()
