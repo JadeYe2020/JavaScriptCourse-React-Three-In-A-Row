b697366b4038c65f694c56d60eede70de604ff51
Jade Ye <JadeYe2020>
2022-03-15 21:27:45 -0300

    Use DOM to create an empty table with rows.

--- Assignments/Assignment4B/game.js
+++ Assignments/Assignment4B/game.js
@@ -3,7 +3,21 @@
     fetch("https://threeinarowpuzzle.herokuapp.com/sample")
     .then(response => response.json())
     .then(json => {
-        console.log(json);
+        let puzzleTable = document.createElement("table");
+        puzzleTable.id = "theTable";
+        document.querySelector("#theGame").appendChild(puzzleTable);
+
+        //let rowCount = json.rows.length;
+        
+
+        for (let i = 0; i < json.rows.length; i++) {
+            let puzzleRow = document.createElement("tr");
+            puzzleRow.id = "#row" + (i+1);            
+            document.querySelector("#theTable").appendChild(puzzleRow);
+        }
+
+
+        //console.log(rowCount);
     });
 
 })();
