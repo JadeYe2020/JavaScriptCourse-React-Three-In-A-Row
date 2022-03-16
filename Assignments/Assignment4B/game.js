b49e8bd66e6dde413d88e4688902c876b106cd45
Jade Ye <JadeYe2020>
2022-03-15 22:46:31 -0300

    Set up grids.

--- Assignments/Assignment4B/game.js
+++ Assignments/Assignment4B/game.js
@@ -7,13 +7,27 @@
         puzzleTable.id = "theTable";
         document.querySelector("#theGame").appendChild(puzzleTable);
 
-        //let rowCount = json.rows.length;
+        let gridSize = json.rows.length;
         
 
-        for (let i = 0; i < json.rows.length; i++) {
+        for (let i = 0; i < gridSize; i++) {
             let puzzleRow = document.createElement("tr");
-            puzzleRow.id = "#row" + (i+1);            
+            puzzleRow.id = "row" + i;            
             document.querySelector("#theTable").appendChild(puzzleRow);
+
+            for (let j = 0; j < gridSize; j++) {
+                let puzzleGrid = document.createElement("td");
+                
+                if (json.rows[i][j].canToggle) {
+                    puzzleGrid.id = "fixed";                    
+                } else {
+                    puzzleGrid.id = "notFixed";
+                }
+
+                puzzleGrid.className = "state" + json.rows[i][j].currentState;
+
+                document.querySelector("#row" + i).appendChild(puzzleGrid);
+            }
         }
 
 
