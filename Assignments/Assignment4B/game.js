4b83b22326c1cffa8c5f7e9f934d8167e2fe4f94
Jade Ye <JadeYe2020>
2022-03-15 23:17:25 -0300

    Put in tbody element between table and tr

--- Assignments/Assignment4B/game.js
+++ Assignments/Assignment4B/game.js
@@ -7,13 +7,15 @@
         puzzleTable.id = "theTable";
         document.querySelector("#theGame").appendChild(puzzleTable);
 
-        let gridSize = json.rows.length;
-        
+        let tableBody = document.createElement("tbody");
+        puzzleTable.appendChild(tableBody);
+
+        let gridSize = json.rows.length;        
 
         for (let i = 0; i < gridSize; i++) {
             let puzzleRow = document.createElement("tr");
             puzzleRow.id = "row" + i;            
-            document.querySelector("#theTable").appendChild(puzzleRow);
+            tableBody.appendChild(puzzleRow);
 
             for (let j = 0; j < gridSize; j++) {
                 let puzzleGrid = document.createElement("td");
