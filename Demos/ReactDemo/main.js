eb407ce53e2e3568d5d5d2d274a9816a20471fd9
Jade Ye <JadeYe2020>
2022-03-29 11:38:10 -0300

    Use a function to return a component

--- Demos/ReactDemo/main.js
+++ Demos/ReactDemo/main.js
@@ -12,10 +12,15 @@
         fontStyle: "italic"
     };
 
+    const myMessage = () => {
+        alert("h3 is clicked!");
+    };
 
-    const Component1 = 
-        <div id="test" style={ myStyles }>
-            <h3>Welcome to Component 1</h3>
+    const Component1 = () => {
+        return (
+            // className, instead of class
+            <div id="test" style={ myStyles } className="blah">
+            <h3 onClick={ myMessage }>Welcome to Component 1</h3>
             <p>React Competitors</p>
             <ul>
                 <li>Angular</li>
@@ -25,9 +30,25 @@
 
             {/* javascript inside curl brackets */}
             {alert("got here")}
-        </div>;
+            </div>
+        );
+    }
+    // const Component1 = 
+    //     <div id="test" style={ myStyles }>
+    //         <h3 onClick={ myMessage }>Welcome to Component 1</h3>
+    //         <p>React Competitors</p>
+    //         <ul>
+    //             <li>Angular</li>
+    //             <li>Ember</li>
+    //             <li>Vue</li>
+    //         </ul>
+
+    //         {/* javascript inside curl brackets */}
+    //         {alert("got here")}
+    //     </div>;
         
 
-    ReactDOM.render(Component1, app);
+    ReactDOM.render(Component1(), app);
+    // ReactDOM.render(Component1, app);
 
 })();
