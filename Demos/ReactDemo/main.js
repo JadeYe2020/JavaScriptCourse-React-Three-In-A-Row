282510cae22fea56a02c7d675897584c88d164e1
Jade Ye <JadeYe2020>
2022-03-29 11:00:10 -0300

    install package json

---
+++ Demos/ReactDemo/main.js
@@ -0,0 +1,10 @@
+(() => {
+
+    // pure - vanilla javaScript
+    // let divNote = document.getElementById("target");
+    // divNote.innerText = "Hello, from Vanilla JS";
+
+    // react way
+    ReactDOM.render(e(LikeButton), domContainer);
+
+})();
