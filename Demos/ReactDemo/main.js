d88ed5a0d3b2f181d4c8d7c519bdcee2fd0c0243
Jade Ye <JadeYe2020>
2022-03-29 11:30:13 -0300

    Use Bable

--- Demos/ReactDemo/main.js
+++ Demos/ReactDemo/main.js
@@ -4,7 +4,30 @@
     // let divNote = document.getElementById("target");
     // divNote.innerText = "Hello, from Vanilla JS";
 
-    // react way
-    ReactDOM.render(e(LikeButton), domContainer);
+    // React way
+
+    // JSX
+    const myStyles = {
+        backgroundColor: "red",
+        fontStyle: "italic"
+    };
+
+
+    const Component1 = 
+        <div id="test" style={ myStyles }>
+            <h3>Welcome to Component 1</h3>
+            <p>React Competitors</p>
+            <ul>
+                <li>Angular</li>
+                <li>Ember</li>
+                <li>Vue</li>
+            </ul>
+
+            {/* javascript inside curl brackets */}
+            {alert("got here")}
+        </div>;
+        
+
+    ReactDOM.render(Component1, app);
 
 })();
