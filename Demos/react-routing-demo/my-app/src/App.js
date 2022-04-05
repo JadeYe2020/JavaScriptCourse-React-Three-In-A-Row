0e8897f8a12f0492ff78e907d40a95429b3f4b69
Jade Ye <JadeYe2020>
2022-04-05 11:50:20 -0300

    in-class demo

---
+++ Demos/react-routing-demo/my-app/src/App.js
@@ -0,0 +1,20 @@
+import { Outlet, Link } from "react-router-dom";
+import './App.css';
+import PersonList from './PersonList';
+import {PostList} from './PostList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React API and Routing Demo Page</h1>
+      <nav>
+        <Link to="">Home</Link> |{" "}
+        <Link to="/people">Customers</Link> |{" "}
+        <Link to="/posts">Posts</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
---
+++ Demos/react-routing-demo/my-app/src/PersonList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class PersonList extends React.Component { // class-based component
+
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+      <div>
+          <h3>List of Customers:</h3>
+          <ul>
+            {
+                this.state.persons
+                .map(person =>
+                    <li key={person.id}>{person.name}</li>
+                )
+            }
+          </ul>
+      </div>
+      
+    )
+  }
+}
