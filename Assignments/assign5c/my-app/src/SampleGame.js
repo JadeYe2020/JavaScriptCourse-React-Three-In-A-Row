bb1917a39e57196b3661acbc8512227503d86811
Jade Ye <JadeYe2020>
2022-04-12 22:02:19 -0300

    Create Board and Square components.

--- Assignments/assign5c/my-app/src/SampleGame.js
+++ Assignments/assign5c/my-app/src/SampleGame.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from 'axios';
+import { Board } from './Board';
 
 export class SampleGame extends React.Component {
     state = {
---
+++ Assignments/assign5c/my-app/src/Square.js
@@ -0,0 +1,8 @@
+export function Square(props) {
+
+    return (
+        <button 
+        onClick={props.onClick}
+        ></button>
+    );
+}
